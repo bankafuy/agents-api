f3bfad6d0ba869659ca10d4259d5440ee9e67400
Full Name <khotibul@code.id>
2020-08-27 17:44:48 +0700

    change default timezone.

--- src/main/java/com/prudential/demo/DemoApplication.java
+++ src/main/java/com/prudential/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.prudential.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,6 +13,11 @@ public class DemoApplication {
 		System.out.println("Starting...");
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Started...");
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
 	}
 
 }
